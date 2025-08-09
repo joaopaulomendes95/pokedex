6b6ad31a7f96724d95334931f2a3f30728d72b5e
joaopaulomendes95 <139879737+joaopaulomendes95@users.noreply.github.com>
2025-08-09 16:37:52 +0100

    add: repl.ts and repl.test.ts

---
+++ src/repl.test.ts
@@ -0,0 +1,42 @@
+import { describe, expect, test } from "vitest";
+import { cleanInput } from "./repl";
+
+describe.each([
+    {
+        input: "  hello  world  ",
+        expected: ["hello", "world"],
+    },
+    {
+        input: "  hello  world  ",
+        expected: ["hello", "world"],
+    },
+    {
+        input: "  what  the hell is going on?  ",
+        expected: ["what", "the", "hell", "is", "going", "on?"],
+    },
+    {
+        input: "  He's long gone ",
+        expected: ["he's", "long", "gone"],
+    },
+    {
+        input: "  It's your opinion  ",
+        expected: ["it's", "your", "opinion"],
+    },
+    {
+        input: "  this testing thing is lame... ",
+        expected: ["this", "testing", "thing", "is", "lame..."],
+    },
+    // TODO: more test cases here
+])("cleanInput($input)", ({ input, expected }) => {
+    test(`Expected: ${expected}`, () => {
+        const actual = cleanInput(input);
+
+        // The `expect` and `toHaveLength` functions are from vitest
+        // they will fail the test if the condition is not met
+        expect(actual).toHaveLength(expected.length);
+        for (const i in expected) {
+            // likewise, the `toBe` function will fail the test if the values are not equal
+            expect(actual[i]).toBe(expected[i]);
+        }
+    });
+});
